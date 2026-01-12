9950e2077472be07c15d427c1a44078057a9ae39
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-12 14:28:40 +0530

    Create setup_db.sql

---
+++ sql/setup_db.sql
@@ -0,0 +1,5 @@
+-- sql/setup_db.sql
+CREATE DATABASE IF NOT EXISTS maindb;
+-- CREATE USER IF NOT EXISTS 'main_user'@'%' IDENTIFIED BY 'Maindb@12345';
+-- GRANT ALL PRIVILEGES ON jenkins_db.* TO 'liquibase_user'@'%';
+-- FLUSH PRIVILEGES;
