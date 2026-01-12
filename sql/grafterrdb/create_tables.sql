a782498c87fbfae7170f9f05d76759b8a8d5577f
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-01-12 21:17:16 +0530

    Update create_tables.sql

--- sql/grafterrdb/create_tables.sql
+++ sql/grafterrdb/create_tables.sql
@@ -1,11 +1,11 @@
 USE grafterrdb;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS grafterrtable (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50),
     email VARCHAR(100)
 );
 
-INSERT INTO users (username, email) VALUES
+INSERT INTO grafterrtable (username, email) VALUES
 ('ketki', 'ketki@enreap.com'),
 ('piyush', 'piyush@enreap.com');
